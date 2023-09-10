359b9ebf1c67f99e968d9a93d0d0de8143f1d2e6
veronicamoreno84 <veronicamoreno84@gmail.com>
2023-09-10 18:36:03 -0300

    Agrego un parametro a bisec que me deja elegir con que mse iniciar la biseccion, 'todas' significa con todas las variables con las que puedo fitear ols, y lasso.min es el mse de ols fiteado con las variables que me devuelva lasso.min

--- LASSO_bisec.py
+++ LASSO_bisec.py
@@ -10,62 +10,88 @@ from sklearn.model_selection import KFold
 import matplotlib.pyplot as plt
 
 
-def seleccion_variables_bis(X,y,eps = 0.05):
+def seleccion_variables_bis(X,y,eps = 0.05,mse='todas'):
     n = len(y)
     frecuencias = area_tray_coef_lasso(X,y)
     best_lambda, best_score, lambda_1se = best_lasso(X,y)
     indeces_ordenan_frecuencias = np.argsort(frecuencias).tolist()
     indeces_ordenan_frecuencias.reverse()
-    clf = Lasso(alpha=best_lambda, fit_intercept=False)
-    clf.fit(X, y)
-    coeficientes = clf.coef_
-    selected_features_LASSOMIN = [i for i in range(len(coeficientes)) if abs(coeficientes[i]) > 0.00001]
-    a = 0
-    if len(selected_features_LASSOMIN) < n:
-        b = len(selected_features_LASSOMIN)-1
-    else:
-        b = p-1
-    mse = 0
-    kf = KFold(n_splits=5)
-    for train, test in kf.split(X):
-        X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
+    if mse =='LASSO.MIN':
+        clf = Lasso(alpha=best_lambda, fit_intercept=False)
+        clf.fit(X, y)
+        coeficientes = clf.coef_
+        selected_features_LASSOMIN = [i for i in range(len(coeficientes)) if abs(coeficientes[i]) > 0.00001]
+        a = 0
+        if len(selected_features_LASSOMIN) < n:
+            b = len(selected_features_LASSOMIN)-1
+        else:
+            b = p-1
+        mse = 0
+        kf = KFold(n_splits=5)
+        for train, test in kf.split(X):
+            X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
 
-        X_sel_train = [[X_train[i][j] for j in selected_features_LASSOMIN] for i in range(len(y_train))]
-        reg = LinearRegression(fit_intercept=False).fit(X_sel_train, y_train)
-        X_sel_test = [[X_test[i][j] for j in selected_features_LASSOMIN] for i in range(len(y_test))]
-        y_pred = reg.predict(X_sel_test)
-        mse += mean_squared_error(y_test, y_pred)
-    mse = mse/ 5
-    stop = False
+            X_sel_train = [[X_train[i][j] for j in selected_features_LASSOMIN] for i in range(len(y_train))]
+            reg = LinearRegression(fit_intercept=False).fit(X_sel_train, y_train)
+            X_sel_test = [[X_test[i][j] for j in selected_features_LASSOMIN] for i in range(len(y_test))]
+            y_pred = reg.predict(X_sel_test)
+            mse += mean_squared_error(y_test, y_pred)
+        mse = mse/ 5
+        stop = False
 
-    cant_variables = len(selected_features_LASSOMIN)
+        cant_variables = len(selected_features_LASSOMIN)
+    if mse == 'todas':
+        if p<n:
+            b = p-1
+        else:
+            b = n -1
+
+        a = 0
+        mse = 0
+        selected_vars = indeces_ordenan_frecuencias[:b]
+        kf = KFold(n_splits=5)
+        for train, test in kf.split(X):
+            X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
+
+            X_sel_train = [[X_train[i][j] for j in selected_vars] for i in range(len(y_train))]
+            reg = LinearRegression(fit_intercept=False).fit(X_sel_train, y_train)
+            X_sel_test = [[X_test[i][j] for j in selected_vars] for i in range(len(y_test))]
+            y_pred = reg.predict(X_sel_test)
+            mse += mean_squared_error(y_test, y_pred)
+        mse = mse / 5
+        stop = False
+
+        cant_variables = b
 
     while not stop:
         cant_variables_new = int((a+b)/2)
-        selected_var = indeces_ordenan_frecuencias[:cant_variables_new]
-        mse_new = 0
-        for train, test in kf.split(X):
-            X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
-            X_sel_train = [[X_train[i][j] for j in selected_var] for i in range(len(y_train))]
-            reg = LinearRegression(fit_intercept=False).fit(X_sel_train, y_train)
-            X_sel_test = [[X_test[i][j] for j in selected_var] for i in range(len(y_test))]
-            y_pred = reg.predict(X_sel_test)
-            mse_new +=  mean_squared_error(y_test, y_pred)
-        mse_new = mse_new/5
-        if mse_new < mse*(1+eps):
-            #mse = mse_new
-            if cant_variables_new != b:
-                b = cant_variables_new
-                cant_variables = cant_variables_new
+        if cant_variables_new != 0:
+            selected_var = indeces_ordenan_frecuencias[:cant_variables_new]
+            mse_new = 0
+            for train, test in kf.split(X):
+                X_train, X_test, y_train, y_test = X[train], X[test], y[train], y[test]
+                X_sel_train = [[X_train[i][j] for j in selected_var] for i in range(len(y_train))]
+                reg = LinearRegression(fit_intercept=False).fit(X_sel_train, y_train)
+                X_sel_test = [[X_test[i][j] for j in selected_var] for i in range(len(y_test))]
+                y_pred = reg.predict(X_sel_test)
+                mse_new +=  mean_squared_error(y_test, y_pred)
+            mse_new = mse_new/5
+            if mse_new < mse*(1+eps):
+                #mse = mse_new
+                if cant_variables_new != b:
+                    b = cant_variables_new
+                    cant_variables = cant_variables_new
+                else:
+                    stop = True
+            elif cant_variables_new != a:
+                a = cant_variables_new
             else:
                 stop = True
-        elif cant_variables_new != a:
-            a = cant_variables_new
         else:
             stop = True
     return indeces_ordenan_frecuencias[:cant_variables]
 
-def monte_carlo_bisec(scenario, n,p,s,rho=None, cant_clusters = None,cant_sim = 1, eps = 0.05):
+def monte_carlo_bisec(scenario, n,p,s,rho=None, cant_clusters = None,cant_sim = 1, eps = 0.05,mse = 'todas'):
 
 
     if scenario == '1' or scenario == '3':
@@ -88,13 +114,13 @@ def monte_carlo_bisec(scenario, n,p,s,rho=None, cant_clusters = None,cant_sim = 
         X = rb.fit_transform(X)
 
         if scenario == '1' or scenario == '3':
-            selected_features_LASSOAREAS =  seleccion_variables_bis(X,y,eps = eps)
+            selected_features_LASSOAREAS =  seleccion_variables_bis(X,y,eps = eps,mse=mse)
             false_fea_selec_LASSOAREAS = [i for i in selected_features_LASSOAREAS if i not in range(s)]
             cant_false_fea_selec_LASSOAREAS+= len(false_fea_selec_LASSOAREAS)
             true_fea_selec_LASSOAREAS = [i for i in range(s) if i in selected_features_LASSOAREAS]
             cant_true_fea_selec_LASSOAREAS += len(true_fea_selec_LASSOAREAS)
         else:
-            selected_features_LASSOAREAS =  seleccion_variables_bis(X,y,eps = eps)
+            selected_features_LASSOAREAS =  seleccion_variables_bis(X,y,eps = eps,mse=mse)
             for resto in range(cant_clusters):
                 false_fea_selec_LASSO_AREAS = [i for i in selected_features_LASSOAREAS if
                                                (i % cant_clusters) == resto and i not in range(s)]
@@ -117,13 +143,13 @@ def monte_carlo_bisec(scenario, n,p,s,rho=None, cant_clusters = None,cant_sim = 
 
         return mean_cant_false_fea_selec_LASSOAREAS, mean_cant_true_fea_selec_LASSOAREAS
 
-def grafico_montecarlo_bisec(scenario, n_list, p, s, rho=None,cant_clusters= None, cant_sim = 1, eps = 0.05, showfig = False, savefig = False, save_in = None):
+def grafico_montecarlo_bisec(scenario, n_list, p, s, mse='todas',rho=None,cant_clusters= None, cant_sim = 1, eps = 0.05, showfig = False, savefig = False, save_in = None):
 
     if scenario == '1' or scenario == '3':
         true_LASSOAREAS_nlist = []
         false_LASSOAREAS_nlist = []
         for n in n_list:
-            mean_false_LASSOAREAS, mean_true_LASSOAREAS = monte_carlo_bisec(scenario,n,p,s,rho,cant_sim = cant_sim, eps = eps)
+            mean_false_LASSOAREAS, mean_true_LASSOAREAS = monte_carlo_bisec(scenario,n,p,s,rho,cant_sim = cant_sim, eps = eps, mse = mse)
             true_LASSOAREAS_nlist.append(mean_true_LASSOAREAS)
             false_LASSOAREAS_nlist.append(mean_false_LASSOAREAS)
 
@@ -151,7 +177,7 @@ def grafico_montecarlo_bisec(scenario, n_list, p, s, rho=None,cant_clusters= Non
             plt.show()
     else:
         for n in n_list:
-            mean_false_LASSOAREAS, mean_true_LASSOAREAS = monte_carlo_bisec(scenario, n,p,s,rho=rho, cant_clusters =cant_clusters,cant_sim = cant_sim, eps = 0.05)
+            mean_false_LASSOAREAS, mean_true_LASSOAREAS = monte_carlo_bisec(scenario, n,p,s,rho=rho, cant_clusters =cant_clusters,cant_sim = cant_sim, eps = 0.05,mse=mse)
             x = np.arange(cant_clusters)
             fig, ax = plt.subplots()
             ax.bar(x, mean_true_LASSOAREAS, width=1, edgecolor="white", linewidth=0.7)
@@ -175,19 +201,19 @@ def grafico_montecarlo_bisec(scenario, n_list, p, s, rho=None,cant_clusters= Non
 
 
 
-scenario = '2'
+scenario = '1'
 n_list = [100,200,400]
 p = 50
 s = 10
-rho_list = [0.2,0.5,0.9]
+rho_list = [None]
 sigma2 = 0.9
 cant_clusters = 10
-cant_sim = 10
-eps_list= [0.01]
-save_in = r'C:\Vero\ML\codigos_Python\Figuras_paper\LASSOARBIS\SCENARIO%s' %(scenario)
+cant_sim = 1000
+eps_list= [0.0025,0.005,0.01]
+save_in = r'C:\Vero\ML\codigos_Python\Figuras_paper\LASSOARBISmseLASSOMIN\SCENARIO%s' %(scenario)
 for eps in eps_list:
     for rho in rho_list:
-        grafico_montecarlo_bisec(scenario, n_list, p, s, rho=rho,cant_clusters= cant_clusters, cant_sim = cant_sim, eps = eps, showfig = False, savefig = True, save_in = save_in)
+        grafico_montecarlo_bisec(scenario, n_list, p, s,mse = 'LASSO.MIN', rho=rho,cant_clusters= cant_clusters, cant_sim = cant_sim, eps = eps, showfig = False, savefig = True, save_in = save_in)
 
 
 # X, y = scenario_1(n, p, s, sigma2)
